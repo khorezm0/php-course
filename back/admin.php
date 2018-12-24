d7fd6c9567b37ef889c616a8dd2943e0a85c41c9
Akmal kamalov <khorezm0@yandex.ru>
2018-12-24 22:37:19 +0400

    proccess

---
+++ back/admin.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once 'header.php';
+require_once 'db.php';
+require_once 'users.php';
+require_once 'utils.php';
+
+if(!$_SESSION["isAdmin"]){
+	header("Location: login.php?admin");
+	die();
+}
+
+$createRow = parseGet($_GET["create"]);
+$editRow = parseGet($_GET["edit"]);
+$delRow = parseGet($_GET["del"]);
+$value = parseGet(urldecode($_GET["value"]));
+
+if($createRow && $value){
+    $q = "INSERT INTO `$createRow` (`name`) VALUES ('$value')";
+    $res = mysql_query($q);
+    if($res){
+        echo (mysql_insert_id());
+    }else{
+        echo 0;
+    }
+}
+if($editRow && $value){
+	$type = parseGet($_GET['type']);
+	if(!$type){
+		die(0);
+	}
+    $q = "UPDATE `$type` SET `name` = '$value' WHERE `id` = '$editRow'";
+    $res = mysql_query($q);
+    if($res){
+        echo 1;
+    }else{
+        echo mysql_error();
+    }
+}
+if($delRow){
+	$type = parseGet($_GET['type']);
+	if(!$type){
+		die(0);
+	}
+    $q = "DELETE FROM `$type` WHERE `id` = '$delRow'";
+    $res = mysql_query($q);
+    if($res){
+        echo 1;
+    }else{
+        echo mysql_error();
+    }
+}
